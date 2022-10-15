0d3cce32a3aec98620ff7488cf83d0ffecc87cf7
Your Name <you@example.com>
2022-10-14 22:16:53 -0300

    actualizaçao projeto api cardapio

---
+++ models/Pedido.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const Pedido = mongoose.model(
+    'Pedido',
+    new Schema({
+        name: {
+            type: String,
+            required: true
+        },
+
+        description: {
+            type: String,
+            required: true
+        },
+
+        price: {
+            type: Number,
+            required: true
+        },
+
+
+        veggie: {
+            type: Boolean,
+            required: true,
+        },
+
+
+        prato: {
+            type: Schema.ObjectId,
+            ref: 'Prato'
+        },
+        
+
+        bebida: {
+            type: Schema.ObjectId,
+            ref: 'Bebida'
+        },
+
+        sobremesa: {
+            type: Schema.ObjectId,
+            ref: 'Sobremesa'
+        }, 
+
+        adicional: {
+            type: Schema.ObjectId,
+            ref: 'Adicional'
+        }, 
+        
+    },
+
+
+    { timestamps: true },
+
+    ),
+
+)
+
+
+
+module.exports = Pedido
---
+++ routes/pedidoRoutes.js
@@ -0,0 +1,206 @@
+const router = require('express').Router()
+
+const { application } = require('express')
+const Pedido = require('../models/Pedido')
+const Prato = require('../models/Prato')
+const Bebida = require('../models/Bebida')
+const Sobremesa = require('../models/Sobremesa')
+const Adicional = require('../models/Adicional')
+
+
+
+
+//Post
+
+router.post('/pedido', async (req,res) => {
+    // req.body
+
+    const {name, description, price, veggie, prato, bebida, sobremesa, adicional} = req.body
+
+ 
+    
+    const pedido = {
+        name, description, price, veggie, prato, bebida, sobremesa, adicional
+    }
+    
+    try {
+//validação prato
+        const prato1 = await Prato.findOne({ _id: prato })
+        
+    
+        if(!prato1) {
+            res.status(404).json({ message: 'O prato informado não existe, porfavor verifique os dados inseridos e tente novamente'})
+            return
+        }
+
+
+//validação bebida
+        const bebida1 = await Bebida.findOne({ _id: bebida })
+        console.log(bebida1)
+    
+        if(!bebida1) {
+            res.status(404).json({ message: 'A bebida informada não existe, porfavor verifique os dados inseridos e tente novamente'})
+            return
+        } 
+        
+
+//validação sobremesa
+        const sobremesa1 = await Sobremesa.findOne({ _id: sobremesa })
+        console.log(sobremesa1)
+    
+        if(!sobremesa1) {
+            res.status(404).json({ message: 'A sobremesa informada não existe, porfavor verifique os dados inseridos e tente novamente'})
+            return
+        } 
+
+//validação adicional
+        const adicional1 = await Adicional.findOne({ _id: adicional })
+        console.log(adicional1)
+    
+        if(!adicional1) {
+            res.status(404).json({ message: 'o adicional informado não existe, porfavor verifique os dados inseridos e tente novamente'})
+            return
+        }  
+
+
+        await Pedido.create(pedido)
+
+        
+    
+        res.status(200).json({message:'Pedido cadastrado com sucesso' })
+        
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+    
+    })
+
+
+     //Get
+   router.get('/pedidos', async (req, res) => {
+    try {
+
+        const pedido = await Pedido.find().populate('prato').populate('bebida').populate('sobremesa').populate('adicional')
+
+        res.status(200).json(pedido)
+        
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+
+   })
+
+
+   //get por id
+
+   router.get('/pedido/:id', async (req, res) => {
+
+   const id = req.params.id
+   
+   try {
+    const pedido = await Pedido.findOne({ _id: id })
+    
+    
+    if(!pedido) {
+        res.status(404).json({ message: 'O pedido informado não existe, porfavor verifique os dados inseridos e tente novamente'})
+        return
+    }
+
+    res.status(200).json(pedido)
+
+   } catch (error) {
+    res.status(500).json({ error: error})
+   }
+})
+
+//get por name
+router.get('/pedido/name/:name', async (req, res) => {
+    
+    const {name} = req.params
+    
+    try {
+     const pedido = await Pedido.findOne({ name: name })
+     
+     
+        if(!pedido) {
+            res.status(404).json({ message: 'O pedido informado não existe, porfavor verifique os dados inseridos e tente novamente'})
+            return
+        }
+    
+        
+
+        if(name ) {
+            res.status(404).json({ message: 'O pedido informado não existe, porfavor verifique os dados inseridos e tente novamente'})
+            return
+        }
+
+
+        else{
+        res.status(200).json(pedido)
+        }
+
+
+    } catch (error) {
+     res.status(500).json({ error: error})
+    }
+ })
+ 
+
+//atualização de dados
+
+router.put('/pedido/:id', async(req, res) => {
+    
+    const id = req.params.id
+
+    const { name, description, price, veggie } = req.body
+
+    const pedido = {name, description, price, veggie}
+
+    try {
+        const updatedPedido = await Pedido.updateOne({ _id: id }, pedido)
+        
+        if(updatedPedido.matchedCount === 0) {
+         res.status(404).json({ message: "O pedido informado não existe, porfavor verifique os dados inseridos e tente novamente"})   
+         return
+        }
+
+        res.status(200).json ({ message: 'Pedido atualizado com sucesso'})
+
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+ 
+})
+
+
+
+
+//Delete
+
+
+router.delete('/pedido/:id', async (req, res) => {
+
+    const id = req.params.id
+
+    const pedido = await Pedido.findOne({ _id: id })
+    
+    if(!pedido) {
+        res.status(404).json({ message: 'O pedido informado não existe, porfavor verifique os dados inseridos e tente novamente'})
+        return
+    }
+
+    try {
+
+        await Pedido.deleteOne({ _id: id})
+
+        res.status(200).json ({ message: 'Pedido excluido com sucesso'})
+        
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+
+})
+
+
+
+    module.exports = router
